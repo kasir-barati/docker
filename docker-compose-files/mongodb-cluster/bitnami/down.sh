157311bc497d744997546ce8025fe26471f5da0a
Kasir Barati <kasir.barati@gmail.com>
2022-09-05 13:09:52 +0430

    fix: calculate the path dynamically

--- docker-compose-files/mongodb-cluster/bitnami/down.sh
+++ docker-compose-files/mongodb-cluster/bitnami/down.sh
@@ -1,5 +1,8 @@
-docker-compose down --remove-orphans
+#!/bin/bash
 
-docker-compose -f cluster/docker-compose.yaml down --remove-orphans
+SCRIPT=$(realpath -s "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+docker-compose -f ${SCRIPT_PATH}/cluster/docker-compose.yaml down --remove-orphans
 
 docker system prune -f
