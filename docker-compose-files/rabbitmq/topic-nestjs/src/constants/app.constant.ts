f85618f8c6ac872cabbaa9354fb041212010df2e
Kasir Barati <kasir.barati@gmail.com>
2024-10-08 14:55:58 +0200

    chore: use # instead of * as a wildcard in routing key for rabbitmq topic exchange

--- docker-compose-files/rabbitmq/topic-nestjs/src/constants/app.constant.ts
+++ docker-compose-files/rabbitmq/topic-nestjs/src/constants/app.constant.ts
@@ -4,9 +4,12 @@ export const USER_WELCOME_MAIL_QUEUE = 'user.send_welcome_email';
 export const USER_DEREGISTRATION_MAIL_QUEUE = 'user.send_deregistration_email';
 
 // Routing key convention: <entity_name>.<verb>
-export const CREATED_ROUTING_KEY = '*.created';
-export const UPDATED_ROUTING_KEY = '*.updated';
-export const DELETED_ROUTING_KEY = '*.deleted';
+// #: matches zero or more words, e.g., “metrics.#” will match all routing keys that start with “metrics.”
+// *: matches one word, for example, “metrics.*.cpu” will match all routing keys that start with “metrics.” and end in “.cpu”.
+
+export const CREATED_ROUTING_KEY = '#.created';
+export const UPDATED_ROUTING_KEY = '#.updated';
+export const DELETED_ROUTING_KEY = '#.deleted';
 export const USER_CREATED_ROUTING_KEY = 'user.created';
 export const USER_DELETED_ROUTING_KEY = 'user.deleted';
 
